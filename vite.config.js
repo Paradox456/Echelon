5fd31cda76118a510affb92f0e47bf3713789393
Paradox456 <islamannafi@gmail.com>
2025-10-16 22:43:10 -0400

    Connected my backend to MongoDB

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: './frontend', // <- tells Vite the frontend folder is the project root
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './frontend/src'),
+    },
+  },
+});
+
